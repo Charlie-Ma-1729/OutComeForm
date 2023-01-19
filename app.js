c1b574b90eac0da6dc245b42e54a136f52781ba3
phoebehsu0517 <108819673+phoebehsu0517@users.noreply.github.com>
2023-01-19 22:28:58 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,106 +1,3 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-// var users = require('./routes/users');
-
-require('./node_modules/routes')(app);
-var routes = require('./node_modules/routes/index');
-// var users = require('./node_modules/routes/users');
-
-// DataBase 
-var mysql = require("mysql");
-
-var con = mysql.createConnection({
-    host: "localhost",
-    user: "outcome",
-    password: "Outcomeform123!",
-    database: "outcome"
-});
-
-con.connect(function(err) {
-    if (err) {
-        console.log('connecting error');
-        return;
-    }
-    console.log('connecting success');
-});
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-// db state
-app.use(function(req, res, next) {
-    req.con = con;
-    next();
-});
-
-
-app.use('/', routes);
-// app.use('/users', users);
-
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
-
-
-module.exports = app;
-
-/*
-var express = require('express');
-var app = express();
-
-app.get('/', function(req, res){
-    res.send('..')
-});
-
-app.listen(8080, function(){
-    console.log('~');
-})
-*/
-
-/*
 const express = require('express');
 const ejs = require('ejs');
 const path = require('path');
@@ -108,7 +5,7 @@ const bodyparser = require('body-parser');
 const jwt = require('jsonwebtoken');
 const cookieparser = require('cookie-parser');
 const dotenv = require('dotenv');
-//const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 const { config } = require('process');
 const app = express();
 dotenv.config();
@@ -118,7 +15,6 @@ app.set('view engine', 'ejs');
 app.use(bodyparser.urlencoded({ extended: false }))
 app.use(cookieparser());
 
-
 const mongoDB = process.env.DB;
 
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -127,8 +23,7 @@ mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
     })
     .catch((err) => {
         console.log(err)
-})
-
+    })
 
 const Schema = mongoose.Schema;
 
@@ -228,7 +123,7 @@ const ResponseSchema = new Schema(
     })
 
 
-
+/*
 const ResponseSchema = new Schema(
     {
         user: {
@@ -322,7 +217,7 @@ const ResponseSchema = new Schema(
             type:String
         }
     })
-
+*/
 const ResponseData = mongoose.model('responses', ResponseSchema)
 
 
@@ -575,4 +470,3 @@ app.get('/data/:id',  (req, res) => {
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log('Server up and running on ' + process.env.PORT + ' or 8000'));
-*/
